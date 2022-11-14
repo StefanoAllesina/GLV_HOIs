90b8d1df0a8dadea6bae0e5a105684619635e658
pablolich <plechon@ucm.es>
2022-11-14 16:59:59 -0600

    wrote efficient computation of jacobian: jacobian_vec(xstar, A, B)

--- code/jacobian.R
+++ code/jacobian.R
@@ -26,33 +26,33 @@ jacobian_vec = function(xstar, A, B){
 }
 
 ################################################################################
-#Timing each function
-n_vec = seq(10, 500, 100)
-n_n = length(n_vec)
-#initialize time vectors
-t_ind = rep(0, n_n)
-t_vect = rep(0, n_n)
-#calculate jacobian for increasing communnity size
-for (i in seq(n_n)){
-  n = n_vec[i]
-  #sample params
-  for (k in 1:n){
-    B[[k]] = matrix(runif(n^2, 0,1), n, n)
-  }
-  xstar = runif(n, 0, 1)
-  A = matrix(runif(n^2, 0, 1), n, n)
-  #time functions
-  t_0 = Sys.time()
-  jacobian_index(xstar, A, B)
-  t_f = Sys.time()
-  t_ind[i] = as.numeric(t_f - t_0)
-  t_0 = Sys.time()
-  jacobian_vec(xstar, A, B)
-  t_f = Sys.time()
-  t_vect[i] = as.numeric(t_f-t_0)
-}
-
-#plot jacobian computation time vs community size for each function
-plot(n_vec, t_ind, pch = 16)
-points(n_vec, t_vect, pch = 16, col = 'blue')
+# #Timing each function
+# n_vec = seq(10, 500, 100)
+# n_n = length(n_vec)
+# #initialize time vectors
+# t_ind = rep(0, n_n)
+# t_vect = rep(0, n_n)
+# #calculate jacobian for increasing communnity size
+# for (i in seq(n_n)){
+#   n = n_vec[i]
+#   #sample params
+#   for (k in 1:n){
+#     B[[k]] = matrix(runif(n^2, 0,1), n, n)
+#   }
+#   xstar = runif(n, 0, 1)
+#   A = matrix(runif(n^2, 0, 1), n, n)
+#   #time functions
+#   t_0 = Sys.time()
+#   jacobian_index(xstar, A, B)
+#   t_f = Sys.time()
+#   t_ind[i] = as.numeric(t_f - t_0)
+#   t_0 = Sys.time()
+#   jacobian_vec(xstar, A, B)
+#   t_f = Sys.time()
+#   t_vect[i] = as.numeric(t_f-t_0)
+# }
+# 
+# #plot jacobian computation time vs community size for each function
+# plot(n_vec, t_ind, pch = 16)
+# points(n_vec, t_vect, pch = 16, col = 'blue')
 
