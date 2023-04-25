29192f136d701564b072fb75b96527bb9a06741d
pablolich <plechon@ucm.es>
2023-04-25 12:08:14 -0500

    .

--- code/get_all_equilibria.r
+++ code/get_all_equilibria.r
@@ -2,19 +2,19 @@
 #with different constraints
 
 #local paths to be changed by user
-setwd("/Users/pablolechon/Desktop/phd/GLV_HOIs/code")
+#setwd("/Users/pablolechon/Desktop/phd/GLV_HOIs/code")
 #find the path to the julia executable in your machine by running the following 
 #command in a julia REPL
 #julia> println(Sys.BINDIR)
-path_to_julia = "/Applications/Julia-1.8.app/Contents/Resources/julia/bin/julia"
-#path_to_julia = "/Users/pablo/julia-1.8.3/bin/julia"
+#path_to_julia = "/Applications/Julia-1.8.app/Contents/Resources/julia/bin/julia" #for my mac
+path_to_julia = "/home/pablo/julia-1.8.3/bin/julia" #for my desktop
 
 #load auxiliary functions to build parameter sets
 source("build_parameter_sets.r")
 
 #specify simulation number and diversity levels
-simulations = 2
-diversities = rep(c(3, 4), each = simulations)
+simulations = 10
+diversities = rep(c(3, 6, 9), each = simulations)
 
 #sample and stack parameters sets of different communities
 pars = sample_stack_par(diversities, 'symmetric')#A and B are symmetric
